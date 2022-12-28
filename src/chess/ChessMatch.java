3fc7fbea39d50899f9bba7a129050a3d4abc3567
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2022-12-28 16:42:14 -0300

    Implementing possible king moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -59,7 +59,7 @@ public class ChessMatch {
             throw new ChessException("There is no piece on source position");
         }
         if (!board.piece(position).isThereAnyPossibleMove()) {
-            throw new ChessException("There is no possible moves for the chose piece");
+            throw new ChessException("There is no possible moves for the chosen piece");
         }
     }
 
