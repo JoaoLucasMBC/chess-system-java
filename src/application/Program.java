2e15f95947810611f81a74d3bb866de0896288cf
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2022-12-30 13:39:05 -0300

    Stop Stockfish engine at the end of the progrma

--- src/application/Program.java
+++ src/application/Program.java
@@ -128,6 +128,7 @@ public class Program {
                 sc.nextLine();
             }
         }
+        client.stopEngine();
         UI.clearScreen();
         UI.printMatch(chessMatch, captured);
     }
